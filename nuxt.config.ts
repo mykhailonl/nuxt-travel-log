b5b79f864712bb09169856534655a4e4e95a054f
Mykhailo Lapchynskyi <mikhaillapchinskyi@gmail.com>
2026-02-11 16:01:55 +0100

    setup zod

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,8 @@ import { fileURLToPath } from 'node:url'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import './app/lib/env'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
